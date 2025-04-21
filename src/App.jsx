d44a66f22b6362b54a5f83d01c42ec3cf4e33633
Argha Guha <arghaguha3001@gmail.com>
2025-04-21 11:51:45 +0530

    Set Up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Board from './components/Board.jsx'
+function App() {
+  
+
+  return (
+    <>
+         <div className="text-3xl font-bold text-blue-600 text-center mt-10">Hello</div>
+         <Board/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Board.jsx
@@ -0,0 +1,31 @@
+
+import { useEffect, useRef } from 'react'
+import rough from "roughjs"
+
+function Board() {
+  const canvasRef = useRef();
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const context = canvas.getContext('2d');
+
+    const roughCanvas = rough.canvas(canvas);
+    const generator = roughCanvas.generator;
+    let rect1 = generator.rectangle(10, 10, 100, 100);
+    let rect2 = generator.rectangle(10, 120, 100, 100, {fill: "red" , stroke: "blue"});
+    roughCanvas.draw(rect1);
+    roughCanvas.draw(rect2);
+
+    // context.fillStyle = '#FF0000';
+    // context.fillRect(0, 0, 150, 75);
+  },[])
+
+  return (
+    <canvas ref={canvasRef}>
+        
+    </canvas>
+  )
+}
+
+export default Board
